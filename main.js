c3515f2bb66289564ff5cb0256b5d64207914021
Kat Wlodarczyk <katbwlodarczyk@gmail.com>
2020-03-22 19:16:53 +0000

    form validation

--- main.js
+++ main.js
@@ -1,3 +1,44 @@
+// FORM VALIDATION 
+
+function main() {
+    console.log("in main function");
+    var Form = document.getElementById("contactForm");
+    Form.addEventListener("submit", validateForm);
+}
+
+//validate callback function 
+function validateForm(event) {
+    var formValid = true;
+    var contactForm = document.getElementById("contactForm");
+
+    if (contactForm.name.value == "") {
+        formValid = false;
+        document.getElementById("nameError").style.display = "block";
+        event.preventDefault();
+    } else {
+        document.getElementById("nameError").style.display = "none";
+    }
+
+    if (contactForm.email.value == "") {
+        formValid = false; 
+        document.getElementById("emailError").style.display = "block";
+        event.preventDefault();
+    } else {
+        document.getElementById("emailError").style.display = "none";
+    }
+
+    if (contactForm.message.value == "") {
+        formValid = false;
+        //display error message 
+        document.getElementById("messageError").style.display = "block";
+        //stop form from submitting
+        event.preventDefault();
+    } else {
+        document.getElementById("messageError").style.display = "none";
+    }
+
+} 
+
 
 //  TYPEWRITER
 document.addEventListener('DOMContentLoaded', function (event) {
@@ -30,5 +71,3 @@ document.addEventListener('DOMContentLoaded', function (event) {
     }
     StartTextAnimation(0);
 });
-
-
